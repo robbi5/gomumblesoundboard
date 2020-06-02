804c1a335e9f4438569b085eebd65fc5bfa37ba7
Markus Witt <feuerrot@feuerrot.org>
2020-06-02 23:52:13 +0200

    feat: generate assets using go generate

---
+++ tools/tools.go
@@ -0,0 +1,7 @@
+// +build tools
+
+// Package tools tracks dependencies for build tools
+// See https://github.com/go-modules-by-example/index/blob/master/010_tools/README.md
+package tools
+
+import _ "github.com/jessevdk/go-assets-builder"
