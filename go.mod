cf274673a411f6de008f10880999ad7aa33ecf45
Markus Witt <feuerrot@feuerrot.org>
2020-06-02 02:14:49 +0200

    feat: break everything but fix the build process

--- go.mod
+++ go.mod
@@ -7,5 +7,5 @@ require (
 	github.com/go-martini/martini v0.0.0-20170121215854-22fa46961aab
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/layeh/gopus v0.0.0-20161224163843-0ebf989153aa // indirect
-	github.com/layeh/gumble v0.0.0-20150101151939-fdf1c7d8480d
+	layeh.com/gumble v0.0.0-20200528195103-d5a9569f0683
 )
--- gomumblesoundboard.go
+++ gomumblesoundboard.go
@@ -11,98 +11,92 @@ import (
 	"strings"
 
 	"github.com/go-martini/martini"
-	"github.com/layeh/gumble/gumble"
-	"github.com/layeh/gumble/gumble_ffmpeg"
-	"github.com/layeh/gumble/gumbleutil"
+	"layeh.com/gumble/gumble"
+	"layeh.com/gumble/gumbleffmpeg"
+	"layeh.com/gumble/gumbleutil"
 )
 
 func main() {
-	files := make(map[string]string)
-	var stream *gumble_ffmpeg.Stream
 	targetChannel := flag.String("channel", "Root", "channel the bot will join")
+	files := make(map[string]string)
+	var volume float32 = 0.5
 
-	gumbleutil.Main(func(_ *gumble.Config, client *gumble.Client) {
-		var err error
-		stream, err = gumble_ffmpeg.New(client)
-		if err != nil {
-			fmt.Printf("%s\n", err)
-			os.Exit(1)
-		}
+	gumbleutil.Main(
+		gumbleutil.AutoBitrate,
+		gumbleutil.Listener{
+			Connect: func(e *gumble.ConnectEvent) {
+				var stream *gumbleffmpeg.Stream
 
-		stream.SetVolume(1.0)
-
-		for _, file := range flag.Args() {
-			key := filepath.Base(file)
-			files[key] = file
-		}
-	}, gumbleutil.Listener{
-		// Connect event
-		Connect: func(e *gumble.ConnectEvent) {
-			fmt.Printf("GoMumbleSoundboard loaded (%d files)\n", len(files))
-			fmt.Printf("Connected to %s\n", e.Client.Conn().RemoteAddr())
-			if e.WelcomeMessage != "" {
-				fmt.Printf("Welcome message: %s\n", e.WelcomeMessage)
-			}
-			fmt.Printf("Current Channel: %s\n", e.Client.Self().Channel().Name())
-
-			if *targetChannel != "" && e.Client.Self().Channel().Name() != *targetChannel {
-				channelPath := strings.Split(*targetChannel, "/")
-				target := e.Client.Self().Channel().Find(channelPath...)
-				if target == nil {
-					fmt.Printf("Cannot find channel named %s\n", *targetChannel)
-					os.Exit(1)
-				}
-				e.Client.Self().Move(target)
-				fmt.Printf("Moved to: %s\n", target.Name())
-			}
-
-			// Start webserver
-			m := martini.Classic()
-			// martini.Static() is used, so public/index.html gets automagically served
-			m.Get("/files.json", func() string {
-				keys := make([]string, 0, len(files))
-				for k := range files {
-					keys = append(keys, k)
-				}
-				// Sort keys into alphabetical order. Sick of things moving around
-				ss := sort.StringSlice(keys)
-				ss.Sort()
-
-				js, _ := json.Marshal(ss)
-				return string(js)
-			})
-			m.Get("/play/:file", func(params martini.Params) (int, string) {
-				file, ok := files[params["file"]]
-				if !ok {
-					return 404, "not found"
-				}
-				stream.Stop()
-				err := stream.Play(file)
-				if err != nil {
-					return 400, fmt.Sprintf("%s\n", err)
+				for _, file := range flag.Args() {
+					key := filepath.Base(file)
+					files[key] = file
 				}
 
-				return 200, fmt.Sprintf("Playing %s\n", file)
-			})
-			m.Get("/volume/:volume", func(params martini.Params) (int, string) {
-				strVol := params["volume"]
-				vol, err := strconv.Atoi(strVol)
-				if err != nil {
-					return 400, "NaN"
+				fmt.Printf("GoMumbleSoundboard loaded (%d files)\n", len(files))
+				fmt.Printf("Connected to %s\n", e.Client.Conn.RemoteAddr())
+				fmt.Printf("Current Channel: %s\n", e.Client.Self.Channel.Name)
+
+				if *targetChannel != "" && e.Client.Self.Channel.Name != *targetChannel {
+					channelPath := strings.Split(*targetChannel, "/")
+					target := e.Client.Self.Channel.Find(channelPath...)
+					if target == nil {
+						fmt.Printf("Cannot find channel named %s\n", *targetChannel)
+						os.Exit(1)
+					}
+					e.Client.Self.Move(target)
+					fmt.Printf("Moved to: %s\n", target.Name)
 				}
 
-				if vol < 0 && vol > 100 {
-					return 400, "Number too small or too large"
-				}
+				// Start webserver
+				m := martini.Classic()
+				// martini.Static() is used, so public/index.html gets automagically served
+				m.Get("/files.json", func() string {
+					keys := make([]string, 0, len(files))
+					for k := range files {
+						keys = append(keys, k)
+					}
+					// Sort keys into alphabetical order. Sick of things moving around
+					ss := sort.StringSlice(keys)
+					ss.Sort()
 
-				stream.SetVolume(float32(vol) / 100)
-				return 200, fmt.Sprintf("Volume set to %d\n", vol)
-			})
-			m.Get("/stop", func() string {
-				stream.Stop()
-				return "ok"
-			})
-			m.Run()
-		},
-	})
+					js, _ := json.Marshal(ss)
+					return string(js)
+				})
+				m.Get("/play/:file", func(params martini.Params) (int, string) {
+					file, ok := files[params["file"]]
+					if !ok {
+						return 404, "not found"
+					}
+					stream := gumbleffmpeg.New(e.Client, gumbleffmpeg.SourceFile(file))
+					stream.Volume = volume
+					err := stream.Play()
+					if err != nil {
+						return 400, fmt.Sprintf("%s\n", err)
+					}
+					return 200, fmt.Sprintf("Playing %s\n", file)
+				})
+				m.Get("/volume/:volume", func(params martini.Params) (int, string) {
+					strVol := params["volume"]
+					vol, err := strconv.Atoi(strVol)
+					if err != nil {
+						return 400, "NaN"
+					}
+
+					if vol < 0 && vol > 100 {
+						return 400, "Number too small or too large"
+					}
+
+					volume = float32(vol) / 100
+					return 200, fmt.Sprintf("Volume set to %d\n", vol)
+				})
+				m.Get("/stop", func() string {
+					stream.Stop()
+					return "ok"
+				})
+				m.Run()
+			},
+			Disconnect: func(e *gumble.DisconnectEvent) {
+				os.Exit(1)
+			},
+		})
 }
